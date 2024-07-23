b8f0ab7023bab6aa01053b48677dfaa0c8195948
piyush <piyushthummar7@gmail.com>
2024-07-23 19:32:14 +0530

    Added all Resources

--- app/src/main/java/com/piyushjt/todo/ui/theme/Color.kt
+++ app/src/main/java/com/piyushjt/todo/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.piyushjt.todo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// All Required Colors
+val Background = Color(0xFFF1F5F9)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Text = Color(0xFF1B1B1D)
+val LightText = Color(0xFF5F5F61)
+val CanceledText = Color(0xFF8D8D8E)
+val LightCanceledText = Color(0xFFAFAFB0)
+val Purple = Color(0xFF4A3780)
